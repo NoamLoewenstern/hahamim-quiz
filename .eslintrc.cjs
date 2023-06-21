3ee6abb9748b511f1baf8f7d01da78ecc08726eb
Noam Loewenstern <noamloewenstern@gmail.com>
2023-06-21 14:02:35 +0300

    refactored prettier-linting + eslint configuration.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
   overrides: [
     {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:prettier/recommended",
-        "stylelint-prettier/recommended",
-        "stylelint-config-prettier",
-      ],
+      extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
+      },
+      rules: {
+        "@typescript-eslint/no-unsafe-assignment": "warn",
       },
     },
   ],
@@ -20,8 +19,19 @@ const config = {
   parserOptions: {
     project: path.join(__dirname, "tsconfig.json"),
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  settings: {
+    next: {
+      rootDir: "/src/",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "autofix", "prettier"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -30,9 +40,15 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "react-refresh/only-export-components": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_+$" }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-misused-promises": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
   },
+  reportUnusedDisableDirectives: true,
+  root: true,
 };
 
 module.exports = config;
